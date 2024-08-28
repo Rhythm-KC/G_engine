ed6638b25d9d06830bf6f09bba3d370471dc4a64
Rhythm-KC <rhythmkc7@gmail.com>
2024-08-28 16:07:54 -0400

    Initial update with test template

---
+++ src/Test/adder.cpp
@@ -0,0 +1,5 @@
+#include "Test/adder.h"
+
+int add(int a, int b){
+    return a + b;
+}
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "Test/adder.h"
+int main(){
+    int x = add(2, 5);
+   std::cout << "hello world" << x << std::endl;
+   return 0;
+}
